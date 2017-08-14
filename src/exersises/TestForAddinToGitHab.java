c6c1cac1b6d92fafff39afd4cbf65357b6e0f2ca
JonnyDem <tl-k@ukr.net>
2017-08-14 23:23:56 +0300

    Class was added

--- src/exersises/TestForAddinToGitHab.java
+++ src/exersises/TestForAddinToGitHab.java
@@ -3,5 +3,6 @@ package exersises;
 public class TestForAddinToGitHab {
     public static void main(String[] args) {
         System.out.println("This class was added to reposetry");
+        System.out.println("changes");
     }
 }
